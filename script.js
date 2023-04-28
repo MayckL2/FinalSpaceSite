f26e7f5480a83d1f2b96784b133e1e51026f9bd9
Mayck <103364944+MayckL2@users.noreply.github.com>
2023-04-28 00:07:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+// fecha tela de carregamento
+function delay(){
+    setTimeout(fechaCarregando, 2000)
+}
+
+function fechaCarregando(){
+    document.querySelector(".carregando").style.display= "none"
+}
